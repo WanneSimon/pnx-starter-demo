3b2b29655e7525b2b7e764d6d3f5d2d3c7e18745
wanne <wanneseville@foxmail.com>
2022-08-05 11:27:04 +0800

    change pom structure

--- src/main/java/cc/wanforme/nukkit/springDemo/AppDemo.java
+++ src/main/java/cc/wanforme/nukkit/springDemo/AppDemo.java
@@ -7,14 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import cc.wanforme.nukkit.spring.NukkitApplicationLauncher;
 
 @SpringBootApplication
-public class StarterDemo extends NukkitApplicationLauncher{
-	private static final Logger log = LoggerFactory.getLogger(StarterDemo.class);
+public class AppDemo extends NukkitApplicationLauncher{
+	private static final Logger log = LoggerFactory.getLogger(AppDemo.class);
 	
 	public static void main(String[] args) {
 		log.info("<<<<<<<<<<<<<<<< starting ! >>>>>>>>>>>>>>>");
 //		SpringApplication.run(StarterDemo.class, args);
-		launchNukkit(StarterDemo.class, args);
-		log.info("\n<<<<<<<<<<<<<<<<\tweb server started\t>>>>>>>>>>>>>>>");
+		launchNukkit(AppDemo.class, args);
+		log.info("\n<<<<<<<<<<<<<<<<\tproject server started\t>>>>>>>>>>>>>>>");
 	}
 	
 }
