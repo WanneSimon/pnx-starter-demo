d61758d2ec09ec70941ebe6c228206b2cdf051d3
wanne <wanneseville@foxmail.com>
2023-11-09 14:06:38 +0800

    wuwa

--- run.bat
+++ run.bat
@@ -1,4 +1,9 @@
 cd target
 set JAVA17="C:\Program Files\Java\jdk-17\bin\java.exe"
- %JAVA17% -jar --add-opens java.base/java.lang=ALL-UNNAMED pnx-starter-demo-1.0-SNAPSHOT.jar
+%JAVA17%  --add-opens java.base/java.lang=ALL-UNNAMED ^
+-cp ./libs/*;./pnx-starter-demo-2.0-SNAPSHOT-exec.jar ^
+org.springframework.boot.loader.JarLauncher
+@rem %JAVA17%  --add-opens java.base/java.lang=ALL-UNNAMED ^
+@rem -cp ./libs/*;./libs-app/*;./powernukkitx.jar;./pnx-starter-demo-1.0-SNAPSHOT-exec.jar ^
+@rem org.springframework.boot.loader.JarLauncher
 PAUSE
